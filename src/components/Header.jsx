cae46c2166e3ea5950e474841fdf1214af1ee3fc
Isha Dabas <83919888+IshaDabas@users.noreply.github.com>
2025-07-26 16:05:32 -0400

    removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../styles/theme.css";
-import SearchModal from "../modals/searchModal";
+import SearchModal from "../modals/SearchModal";
 import { useNavigate } from "react-router-dom";
 import { Typography } from "@visa/nova-react";
 
